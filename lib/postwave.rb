7fc08ff9f31c70381a36d73869e5820d2f779d13
Dave Schwantes <dave.schwantes@gmail.com>
2022-03-07 21:19:00 -0600

    got the basic CLI working

--- lib/postwave.rb
+++ lib/postwave.rb
@@ -1,22 +1,21 @@
 require_relative "postwave/blog_creator"
 require_relative "postwave/blog_builder"
 require_relative "postwave/post_creator"
-require_relative "postwave/post"
 require_relative "postwave/version"
 
 module Postwave
-  def self.call(options)
-    # handle command line options here
-
-    # new
-    #Postwave::BlogCreator.instance.create
-    
-    # post
-    # Postwave::PostCreator.instance.create
-    
-    # build
-    Postwave::BlogBuilder.instance.build
+  def self.call(command, options)
+    case command
+    when "new"
+      Postwave::BlogCreator.instance.create
+    when "post"
+      Postwave::PostCreator.instance.create
+    when "build"
+      Postwave::BlogBuilder.instance.build
+    else
+      if options[:version]
+        puts "postwave #{VERSION} [ruby]"
+      end
+    end
   end
 end
-
-Postwave.call({})
--- lib/postwave/version.rb
+++ lib/postwave/version.rb
@@ -1,3 +1,3 @@
-module Trackstar
+module Postwave
   VERSION = "0.0.1"
 end
